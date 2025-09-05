411a7cfe631d925fd031850491d2c71dd17eb1de
billdeptrai0512 <billnguyen05121998@gmail.com>
2025-09-05 22:40:00 +0700

    early listing verify mail

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -43,14 +43,14 @@ const sendVerifyEmail = async (to, staycation) => {
 
   await transporter.sendMail({
     from: process.env.EMAIL_USER,
-    to,
+    to: "billnguyen05121998@gmail.com", // for early listing
     subject: "Xác minh thông tin liên lạc",
     html: `
       <div style="font-family: Arial, sans-serif; padding: 20px; color: #333; background:#fafafa;">
         <table width="100%" cellpadding="0" cellspacing="0" border="0" style="max-width:600px; margin:auto; background:#fff; border-radius:8px; box-shadow:0 2px 8px rgba(0,0,0,0.1);">
           <tr>
             <td style="padding: 30px; text-align:center; border-bottom:1px solid #eee;">
-              <h2 style="margin:0; font-size:22px; color:#2c3e50;">Xin chào, ${staycation.name}</h2>
+              <h2 style="margin:0; font-size:22px; color:#2c3e50;">Xin chào, ${staycation.name} - ${staycation.id}</h2>
             </td>
           </tr>
           <tr>
