5c530be0266d5d0b473ef2792dd73ca0c9e740b2
billdeptrai0512 <billnguyen05121998@gmail.com>
2025-11-17 16:14:01 +0700

    add allTraffic

--- routes/trafficRouter.js
+++ routes/trafficRouter.js
@@ -4,5 +4,6 @@ const trafficController = require("../controllers/trafficController")
 
 trafficRouter.post("/:staycationId", trafficController.recordStaycationClick);
 trafficRouter.get("/weekly-report/:hostId", trafficController.sendWeeklyPerformance);
+trafficRouter.get("/allTraffic", trafficController.getAllTraffic);
 
 module.exports = trafficRouter;
